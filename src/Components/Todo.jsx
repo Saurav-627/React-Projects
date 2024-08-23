0f1b3d3d65e5de18576ac16d63e1b6f8b665ec94
Saurav Luitel <luitelsaurav627@gmail.com>
2024-08-23 17:47:50 +0545

    Adding Date and Time

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./todo.css";
 import { MdCheck, MdDeleteForever } from "react-icons/md";
 
 const Todo = () => {
   const [inputValue, setInputValue] = useState("");
   const [task, setTask] = useState([]);
+  const [dateTime, setDateTime] = useState("")
 
   const handleInputChange = (value) => {
     setInputValue(value);
@@ -27,10 +28,27 @@ const Todo = () => {
     setInputValue("");
   };
 
+  // Todo Date and Time
+
+  useEffect(() => {
+
+    const interval = setInterval (() => {
+      const now = new Date();
+      const formattedDate = now.toLocaleDateString();
+      const formattedTime = now.toLocaleTimeString('en-US', { hour12: true });
+      setDateTime(`Date : ${formattedDate} - Time : ${formattedTime}`);
+    }, 1000)
+
+    // cleanup function 
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <section className="flex flex-col items-center bg-gray-950 font-bold text-gray-50 h-screen">
-      <header className="my-7  text-3xl">
+      <header className="my-7  text-3xl text-center">
         <h1>Todo List</h1>
+        <h2 className="text-xl text-center mt-2">{dateTime}</h2>
       </header>
       <section className="">
         <form className="flex" onSubmit={handleFormSubmit}>
@@ -54,7 +72,10 @@ const Todo = () => {
         <ul>
           {task.map((curTask, index) => {
             return (
-              <li key={index} className="flex justify-between m-3 px-4 font-medium bg-slate-100 text-black rounded-full py-2 my-3">
+              <li
+                key={index}
+                className="flex justify-between m-3 px-4 font-medium bg-slate-100 text-black rounded-full py-2 my-3"
+              >
                 <span>{curTask}</span>
                 <div className="flex flex-cols gap-8 text-xl text-white">
                   <button className="radius bg-green-500 p-1">
