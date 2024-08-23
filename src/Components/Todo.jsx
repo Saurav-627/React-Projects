bd56f829991b4740aa3c9d413d528820b53cd8ba
Saurav Luitel <luitelsaurav627@gmail.com>
2024-08-23 21:17:13 +0545

    Added Mark and Unmark

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,9 +3,11 @@ import "./todo.css";
 import { MdCheck, MdDeleteForever } from "react-icons/md";
 
 const Todo = () => {
+  // kehi kura change hudeicha vane always use useState hooks
   const [inputValue, setInputValue] = useState("");
   const [task, setTask] = useState([]);
-  const [dateTime, setDateTime] = useState("")
+  const [dateTime, setDateTime] = useState("");
+  const [isRead, setMarkAsRead] = useState([]);
 
   const handleInputChange = (value) => {
     setInputValue(value);
@@ -31,18 +33,38 @@ const Todo = () => {
   // Todo Date and Time
 
   useEffect(() => {
-
-    const interval = setInterval (() => {
+    const interval = setInterval(() => {
       const now = new Date();
       const formattedDate = now.toLocaleDateString();
-      const formattedTime = now.toLocaleTimeString('en-US', { hour12: true });
+      const formattedTime = now.toLocaleTimeString("en-US", { hour12: true });
       setDateTime(`Date : ${formattedDate} - Time : ${formattedTime}`);
-    }, 1000)
+    }, 1000);
 
-    // cleanup function 
+    // cleanup function
     return () => clearInterval(interval);
   }, []);
 
+  //todo handleDeleteTodo function
+  const handleDeleteTodo = (value) => {
+    const updatedTask = task.filter((curTask) => curTask !== value);
+    setTask(updatedTask);
+  };
+
+  //todo handleClearButton function
+
+  const handleClearButton = () => {
+    setTask([]);
+  };
+
+  //todo handleMarkAsRead function
+  const handleMarkAsRead = (index) => {
+    // setMarkAsRead(index);
+    if (isRead.includes(index)) {
+      setMarkAsRead(isRead.filter((curTask) => curTask !== index));
+    } else {
+      setMarkAsRead([...isRead, index]);
+    }
+  };
 
   return (
     <section className="flex flex-col items-center bg-gray-950 font-bold text-gray-50 h-screen">
@@ -76,12 +98,27 @@ const Todo = () => {
                 key={index}
                 className="flex justify-between m-3 px-4 font-medium bg-slate-100 text-black rounded-full py-2 my-3"
               >
-                <span>{curTask}</span>
+                <span
+                  style={{
+                    textDecoration: isRead.includes(index)
+                      ? "line-through"
+                      : "none",
+                    fontWeight: isRead.includes(index) ? "bold" : "",
+                  }}
+                >
+                  {curTask}
+                </span>
                 <div className="flex flex-cols gap-8 text-xl text-white">
-                  <button className="radius bg-green-500 p-1">
+                  <button
+                    className="radius bg-green-500 p-1"
+                    onClick={() => handleMarkAsRead(index)}
+                  >
                     <MdCheck />
                   </button>
-                  <button className="radius bg-red-600 p-1">
+                  <button
+                    className="radius bg-red-600 p-1"
+                    onClick={() => handleDeleteTodo(curTask)}
+                  >
                     <MdDeleteForever />
                   </button>
                 </div>
@@ -89,6 +126,14 @@ const Todo = () => {
             );
           })}
         </ul>
+        <section className="flex justify-center">
+          <button
+            className="bg-red-600 p-2 rounded-2xl"
+            onClick={handleClearButton}
+          >
+            Clear all
+          </button>
+        </section>
       </section>
     </section>
   );
